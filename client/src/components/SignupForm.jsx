b73ba053206295520bb61744db806733106fd936
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2024-01-05 21:40:42 -0500

    Updated sign up form for apollo

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,10 +12,22 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser,{error, data}] =useMutation(ADD_USER);
+  useEffect(()=>{
+    if(error){
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  },[error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+
+    setUserFormData({ 
+      ...userFormData, 
+      [name]: value 
+    });
   };
 
   const handleFormSubmit = async (event) => {
@@ -28,18 +41,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await addUser({
+        variables:{...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      console.log("login response", data)
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
